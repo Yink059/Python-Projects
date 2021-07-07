b5103ba8354c2e9ce7db5fea354db66318b989ce
Brian S <b.d.sandford@gmail.com>
2021-07-06 17:50:53 -0700

    test2

--- test.py
+++ test.py
@@ -1,36 +1,62 @@
 import csv
 import os
+import math
 
 #change directory
 os.chdir(r"C:\Users\bdsan\Documents\Imet data")
 cwd = os.getcwd()
 
 #create lists & dictionary
-
-vDict = {
- "saveDateTime" : 0,
- "comTime": 1,
- "xqPressureRaw" : 36,
- "xqTempRaw" : 37,
- "xqHumidity" : 38,
- "xqHumidityTemp": 39,
- "xqLogDate" : 40,
- "xqLogTime" : 41,
- "xqLongitude" : 42,    # xxx.xxxx
- "xqLatitude" : 43,     # xxx.xxxx
+rawData = []
+vD = { #value dictionary for indices, values labeled xq are pre-trimmed csv indices
+ "saveDateTime" : 0,    #date+time when data was written to csv
+ "comTime": 1,          #the write time of the data to the csv
+ "Pressure": 2,
+ "temp": 3,
+ "humidity": 4,
+ "humidityTemp": 5,
+ "logDate": 6,
+ "logTime": 7,
+ "longitude": 8,
+ "latitude": 9,
+ "altitude": 10,
+ "satCount": 11,
+ "xqPressureRaw" : 36,  #Air Pressure in mm Hg (Torr)splitList[i][vD["altitude"]]
+ "xqTempRaw" : 37,      #Air temp in centigrade
+ "xqHumidity" : 38,     #Relative Humidity
+ "xqHumidityTemp": 39,  #no clue
+ "xqLogDate" : 40,      #log date in format YYYY/MM/DD
+ "xqLogTime" : 41,      #log time in HH:MM:SS (Zulu)
+ "xqLongitude" : 42,    # xxx.xxxxxx
+ "xqLatitude" : 43,     # xxx.xxxxxx
  "xqAltitude" : 44,     #in meters
- "xqSatCount" : 45
+ "xqSatCount" : 45      # number of connected GPS sats
  }
+dictValues = [i + 2 for i in range(10)]
 
-
-
-file = open("20210706-164825-00055359.csv", newline='')
-
+#open raw csv file and read it
+file = open("20210706-164825-00055359.csv", newline='\n')
 rawList = file.readlines()
+splitList = []
+#print(len(rawList))
 
-logData = []
+for i in range(len(rawList)): #split raw csv into individual lines
+    splitList += rawList[i].split("', '")
+del splitList[0] #delete csv definition line
 
-for i in range(len(rawList)):
-    logData += rawList[i].split(",")
+for i in range(len(splitList)): #split each line into individual lists
+    splitList[i] = splitList[i].split(",")
+    del splitList[i][2:36] #delete the empty values
 
-    print((rawList[i].split(","))[vDict["xqLogTime"]]+ " "+ (rawList[i].split(","))[vDict["xqAltitude"]])
+
+
+    for x in dictValues:
+        print(splitList[i][x], end=', ')
+    print()
+    #print(splitList[i][vD["latitude"]] + " " + splitList[i][vD["longitude"]]+ " " + splitList[i][vD["altitude"]])
+
+
+
+
+
+    #print((rawList[i].split(","))[vDict["xqLogTime"]]+ " "+ (rawList[i].split(","))[vDict["xqAltitude"]])
